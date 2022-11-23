85d6e533e4785152052a1d0d889cfdbc957e5a82
Muhaymin Bin Mehmood <72308336+muhaymin-bin-mehmood@users.noreply.github.com>
2022-11-24 03:26:31 +0500

    added contextAPI on githubListing and alert

---
+++ src/component/users/UserItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropType from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const UserItem = ({ user: { login, avatar_url, id } }) => {
+    return (
+        <div key={id} className='card shadow-md compact side bg-base-100'>
+            <div className='flex-row item-center space-x-4 card-body'>
+                <div>
+                    <div className="avatar">
+                        <div className="rounded-full shadow w-14 h-14">
+                            <img src={avatar_url} alt="profile" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h2 className="card-title">{login}</h2>
+                    <Link to={`/users/${login}`} className='text-base-content text-opacity-40'>Visit Profile</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+UserItem.propType = {
+    user: PropType.object.isRequired
+}
+
+export default UserItem
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import UserResult from '../component/users/UserResult'
+import UserSearch from '../component/users/UserSearch'
 
 const Home = () => {
     return (
         <div>
             {/* Search Component */}
+            <UserSearch />
             <UserResult />
         </div>
     )
